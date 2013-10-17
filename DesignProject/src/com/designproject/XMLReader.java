a92d33516ff1f718894cc624805229c6f4f36b09
NikLubz <nlubczynski@gmail.com>
2013-10-17 15:22:56 -0400

    Adding the start of the XML Parser

---
+++ DesignProject/src/com/designproject/XMLReader.java
@@ -0,0 +1,70 @@
+package com.designproject;
+
+import java.io.IOException;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+/**
+ * 
+ * @author NikLubz and Jessica
+ *
+ */
+public class XMLReader {
+
+	private String pathToXMLFile;
+	private Franchise franchise;
+	private XmlPullParser parser;
+	
+	/**
+	 * Constructor
+	 * @param String pathToXMLFile - the path to the XML file being used
+	 * @throws XmlPullParserException
+	 */
+	public XMLReader(String pathToXMLFile) throws XmlPullParserException{
+		
+		//The path to the XML file
+		this.pathToXMLFile = pathToXMLFile;
+		
+		//Create the factory (temp) to create the parser (member variable)
+		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+		parser = factory.newPullParser();
+	}
+	
+	public Franchise parseXML() throws XmlPullParserException, IOException{
+		
+		Franchise returnVal = null;
+		
+		int eventType = parser.getEventType();
+		
+		while(eventType != XmlPullParser.END_DOCUMENT){
+			
+			switch(eventType){
+			case XmlPullParser.START_DOCUMENT:
+				System.out.println("Starting parsing of document");
+				break;
+			case XmlPullParser.START_TAG:
+				if( parser.getName() == "Franchisee" ) break;
+				else if( parser.getName() == "Client" ) break;
+				else if( parser.getName() == "clientContract" ) break;
+				else if( parser.getName() == "ServiceAddress" ) break;
+				else if( parser.getName() == "Floor" ) break;
+				else if( parser.getName() == "Room" ) break;
+				else if( parser.getName() == "Extinguisher" ) break;
+				else if( parser.getName() == "inspectionElement" ) break;
+				break;
+			case XmlPullParser.END_TAG:
+				break;
+			case XmlPullParser.TEXT:
+				break;			
+			}
+			
+			eventType = parser.next();			
+		}
+		
+		System.out.println("Done parsing document");
+		return returnVal;
+	}
+	
+}
