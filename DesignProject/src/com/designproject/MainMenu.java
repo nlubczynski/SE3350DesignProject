63674ab123441a4bb96e62e6b59ef2c8097ef41a
Braden Lunn <braden.lunn@gmail.com>
2013-10-17 17:12:26 -0400

    checkpoint before I go to table views

--- DesignProject/src/com/designproject/MainMenu.java
+++ DesignProject/src/com/designproject/MainMenu.java
@@ -17,7 +17,6 @@ public class MainMenu extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_menu);
     }
 
