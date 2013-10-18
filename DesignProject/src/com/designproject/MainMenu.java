b543d23320afdb484feac8511946b00c31343249
Braden Lunn <braden.lunn@gmail.com>
2013-10-18 00:15:08 -0400

    Finished the navigation drawer aspect

--- DesignProject/src/com/designproject/MainMenu.java
+++ DesignProject/src/com/designproject/MainMenu.java
@@ -6,26 +6,51 @@ import com.designproject.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainMenu extends Activity {
 
+	private String[] mDrawerListTitles;
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_menu);
+        
+   
+        mDrawerListTitles = getResources().getStringArray(R.array.drawer_list_options);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        
+        // Set the adapater for the list view
+        mDrawerList.setAdapter((new ArrayAdapter<String>(this,
+        		R.layout.drawer_list_item, mDrawerListTitles)));
+        
+        // Set the adapter for this list view
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
-    @Override
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
-        return true;
+		  MenuInflater inflater = getMenuInflater();
+	        inflater.inflate(R.menu.activity_main_menu, menu);
+	        return super.onCreateOptionsMenu(menu);
     }
     
     public void signOut(View view)
@@ -37,5 +62,20 @@ public class MainMenu extends Activity {
     	Intent loginScreen = new Intent(MainMenu.this, LoginScreen.class);
     	startActivity(loginScreen);
     }
+    
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+    
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+
+    }
+    
+    
     
 }
