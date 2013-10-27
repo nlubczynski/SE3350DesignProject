a787e51c21196c581d460b4bd614383b7a19025b
Braden Lunn <braden.lunn@gmail.com>
2013-10-27 14:55:39 -0400

    Updated the way we get the franchsie

--- DesignProject/src/com/designproject/MainMenu.java
+++ DesignProject/src/com/designproject/MainMenu.java
@@ -41,8 +41,7 @@ public class MainMenu extends Activity {
 			XMLReaderWriter reader = new XMLReaderWriter( this.getApplicationContext() );
 			FireAlertApplication a = (FireAlertApplication)getApplication();
 			a.setFranchise( reader.parseXML() );
-			mFranchise = a.getFranchise();
-			reader.writeXML( franchise );
+			reader.writeXML( a.getFranchise() );
 		}catch (XmlPullParserException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
