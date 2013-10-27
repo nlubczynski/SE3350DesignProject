8796208368af66861adc3b00f0609236a2b73a0f
NikLubz <nlubczynski@gmail.com>
2013-10-27 14:03:14 -0400

    Added Global object of current "location"

--- DesignProject/src/com/designproject/FireAlertApplication.java
+++ DesignProject/src/com/designproject/FireAlertApplication.java
@@ -5,12 +5,19 @@ import android.app.Application;
 public class FireAlertApplication extends Application {
 
 	private Franchise franchise;
+	private Object location;
 	
 	public void setFranchise(Franchise f){
 		this.franchise = f;
 	}
 	public Franchise getFranchise(){
 		return franchise;
+	}
+	public void setLocation( Object o ){
+		this.location = o;
+	}
+	public Object getLocation(){
+		return this.location;
 	}
 	
 }
