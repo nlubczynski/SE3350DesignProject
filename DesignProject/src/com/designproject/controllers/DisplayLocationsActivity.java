6753fc85cf1a4174bbead7702aa31d366102c74c
Braden Lunn <braden.lunn@gmail.com>
2013-12-04 00:38:49 -0500

    Commented and cleaned up some classes

--- DesignProject/src/com/designproject/controllers/DisplayLocationsActivity.java
+++ DesignProject/src/com/designproject/controllers/DisplayLocationsActivity.java
@@ -21,64 +21,59 @@ import android.widget.SimpleAdapter;
 /**
  * 
  * @author Jess
- *
+ * 
  */
-public class DisplayLocationsActivity extends Activity {
+public class DisplayLocationsActivity extends NavigationDrawerActivity {
 
 	private Franchise theFranchise;
-	private Client [] clients;
-	private Contract [] contracts;
-	private Building [] buildings;
-	
+	private Client[] clients;
+	private Contract[] contracts;
+	private Building[] buildings;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		
+	public void onCreate(Bundle savedInstanceState) {
+
+		// Set title for activity
 		setTitle("Locations");
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_locations);
-		
+
+		// Get the list view inflated by set content view
 		ListView list = (ListView) findViewById(R.id.android_locationlist);
-		
-		List<Map<String,String>> locationClientList = new ArrayList<Map<String,String>>();
-		
-		//Get the application context
-		FireAlertApplication a = (FireAlertApplication)getApplication();
-		a = (FireAlertApplication)getApplication();
-		theFranchise = (Franchise)a.getFranchise();
+
+		// Initiate a new array
+		List<Map<String, String>> locationClientList = new ArrayList<Map<String, String>>();
+
+		// Get the application context
+		FireAlertApplication a = (FireAlertApplication) getApplication();
+		a = (FireAlertApplication) getApplication();
+		theFranchise = (Franchise) a.getFranchise();
 		clients = theFranchise.getClients();
-		
-		//Add the location and client name for all contracts
-		for (Client theClient : clients)
-		{
+
+		// Bind a location and client name to a map
+		for (Client theClient : clients) {
 			contracts = theClient.getContracts();
-			
-			for (Contract theContract : contracts)
-			{
+
+			for (Contract theContract : contracts) {
 				buildings = theContract.getBuildings();
-				
-				for (Building theBuilding : buildings)
-				{
-					Map<String,String> pairList = new HashMap<String,String>(2);
+
+				for (Building theBuilding : buildings) {
+					Map<String, String> pairList = new HashMap<String, String>(
+							2);
 					pairList.put("location", theBuilding.getAddress());
 					pairList.put("client", theClient.getName());
 					locationClientList.add(pairList);
 				}
 			}
 		}
-		
-		SimpleAdapter adapter = new SimpleAdapter(this, locationClientList, 
-				android.R.layout.simple_list_item_2, 
-				new String[] {"location", "client"}, 
-				new int[] {android.R.id.text1, android.R.id.text2});
-		
-		list.setAdapter(adapter);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.display_locations, menu);
-		return true;
+		// Bind map to the adapter
+		SimpleAdapter adapter = new SimpleAdapter(this, locationClientList,
+				android.R.layout.simple_list_item_2, new String[] { "location",
+						"client" }, new int[] { android.R.id.text1,
+						android.R.id.text2 });
+
+		list.setAdapter(adapter);
 	}
 }
